d2fc10945a55d66f789a4b329d4a61bf5c9eaf8a
Gabriel Candal <gcc@platforme.com>
2019-09-13 09:03:08 +0100

    Fix production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 const config = require("uxf-webpack/config/webpack.config.full");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const info = require("./package.json");
 
@@ -16,6 +17,18 @@ config.output.path = path.join(__dirname, "dist");
 config.output.filename = "ripe-commons-pluginus.min.js?[hash]";
 config.output.library = "RipeCommonsPluginus";
 config.output.publicPath = "/";
+
+config.optimization = {
+    concatenateModules: false,
+    minimizer: [
+        new TerserPlugin({
+            terserOptions: {
+                keep_fnames: true,
+                keep_classnames: true
+            }
+        })
+    ]
+};
 
 config.externals = config.externals || {};
 config.externals.vue = "vue";
