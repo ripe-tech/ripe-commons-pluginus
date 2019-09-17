1bdf4262b372d20a9565bc916f2879bea6d3709c
Gabriel Candal <gcc@platforme.com>
2019-09-17 11:46:05 +0100

    Don't include initials in price if there is none

--- js/base/main.js
+++ js/base/main.js
@@ -251,9 +251,15 @@ class RipeCommonsMainPlugin extends RipeCommonsPlugin {
                 this.$bus.bind("part_change", (part, material, color) =>
                     self.ripe.setPart(part, material, color)
                 );
-                this.$bus.bind("initials_change", (initials, engraving) =>
-                    self.ripe.setInitials(initials, engraving)
-                );
+                this.$bus.bind("initials_change", (initials, engraving) => {
+                    // if both the initials and the engraving are defined,
+                    // apply them. Otherwise remove any initials there might be
+                    if (initials && engraving) {
+                        self.ripe.setInitials(initials, engraving);
+                    } else {
+                        self.ripe.setInitials("", null);
+                    }
+                });
                 this.$bus.bind("undo", () => self.ripe.undo());
 
                 // listens for any model change and triggers the
