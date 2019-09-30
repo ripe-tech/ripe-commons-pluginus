a52435545de7a084ce546fe1dc3e8a9af8e41caf
joamag <joamag@gmail.com>
2019-09-30 16:53:00 +0100

    New naming

--- vue/mixins/logic.js
+++ vue/mixins/logic.js
@@ -69,31 +69,36 @@ const logicMixin = {
                 return true;
             }
 
-            if (!this._subsetInitialsExtra(first, second)) {
+            if (!this._subsetCompare(first, second)) {
                 return true;
             }
 
-            if (!this._subsetInitialsExtra(second, first)) {
+            if (!this._subsetCompare(second, first)) {
                 return true;
             }
 
             return false;
         },
-        _subsetInitialsExtra(base, superset) {
-            for (const group of Object.keys(base)) {
-                const groupBase = base[group];
-                const groupSuperSet = superset[group];
+        _subsetCompare(base, reference) {
+            for (const name of Object.keys(base)) {
+                // retrieves the group information for the current
+                // name in iteration for both the base and the
+                // reference set values (to be compared)
+                const groupB = base[name];
+                const groupR = reference[name];
 
                 // if for a certain base group the corresponding
-                // group does not exist in the super set then the
-                // super set is considered to be invalid
-                if (!groupSuperSet) {
+                // group does not exist in the reference then the
+                // reference is considered to be invalid
+                if (!groupR) {
                     return false;
                 }
 
+                // in case either the initials or the engraving is
+                // not matching then the subset is invalid
                 if (
-                    groupBase.initials !== groupSuperSet.initials ||
-                    groupBase.engraving !== groupSuperSet.engraving
+                    groupB.initials !== groupR.initials ||
+                    groupB.engraving !== groupR.engraving
                 ) {
                     return false;
                 }
