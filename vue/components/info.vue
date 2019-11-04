47f3484e189f85d715d2efd7565dd07289f05b81
3rdvision <kazgru@gmail.com>
2019-11-04 10:03:37 +0000

    refactor

--- vue/components/info.vue
+++ vue/components/info.vue
@@ -2,16 +2,16 @@
     <div class="info">
         <span class="info-button" v-on:click="showModal()" />
         <modal ref="modal" v-bind:visible="true">
-                    <h3 class="title">{{ "ripe_commons.details.title" | locale }}</h3>
-                    <h4 class="subtitle">{{ "ripe_commons.details.subtitle" | locale }}</h4>
-                    <div class="paragraph-container">
-                        <p v-for="(paragraph, index) in detailsText" v-bind:key="`p-${index}`">
-                            {{ paragraph }}
-                        </p>
-                    </div>
-                <div class="button-cancel" v-on:click="hideModal()">
-                    {{ "ripe_commons.modal.got_it" | locale }}
-                </div>
+            <h3 class="title"> Details </h3>
+            <h4 class="subtitle"> Details about MTO </h4>
+            <div class="paragraph-container">
+                <p v-for="(paragraph, index) in paragraphs" v-bind:key="index">
+                    {{ paragraph }}
+                </p>
+            </div>
+            <div class="button-cancel" v-on:click="hideModal()">
+                {{ "ripe_commons.modal.got_it" | locale }}
+            </div>
         </modal>
     </div>
 </template>
@@ -28,13 +28,9 @@
 
 .paragraph-container {
     font-size: 14px;
-    font-stretch: normal;
-    font-style: normal;
-    font-weight: normal;
-    letter-spacing: 1.17px;
-    line-height: normal;
     margin-bottom: 60px;
     text-align: center;
+    line-height: normal;
 }
 
 .button-cancel {
@@ -64,7 +60,6 @@
 .button-cancel:disabled{
     opacity: .2;
 }
-
 </style>
 
 <script>
@@ -72,40 +67,15 @@ import { modalMixin } from "../mixins";
 
 export const info = {
     mixins: [modalMixin],
-    data: function() {
-        return {
-            buttonText: "",
-            paragraph: "",
-            detailsParamText: null
-        };
-    },
-    computed: {
-        detailsText() {
-            return (this.detailsParamText || this.locale("ripe_commons.details.text")).split("|");
+    props: {
+        content: {
+            type: String,
+            mandatory: true
         }
     },
-    created: function() {
-        this.$bus.bind("enable_info", this.enableSize);
-        this.$bus.bind("disable_info", this.disableSize);
-        this.$bus.bind("open_info", callback => {
-            this.closeCallback = callback;
-            this.showModal();
-        });
-        this.$bus.bind("close_info", () => {
-            this.hideModal();
-        });
-    },
-    mounted: function() {
-        this.updateButtonText();
-        this.$bus.bind("refresh", () => {
-            this.updateButtonText();
-        });
-    },
-    methods: {
-        updateButtonText() {
-            this.buttonText = this.infoText
-                ? this.locale("ripe_commons.info.info") + " - " + this.infoText
-                : this.locale("ripe_commons.info.select_info");
+    computed: {
+        paragraphs() {
+            return this.content.split("\n");
         }
     }
 };
