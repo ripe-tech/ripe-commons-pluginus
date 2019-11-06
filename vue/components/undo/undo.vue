25ac31e34fa8163d58443cc0b5e4c685e3924048
Daniel Blazeque <v-db@platforme.com>
2019-11-06 15:54:28 +0000

    Remove CSS from undo component and transfer to ripe white to don't affect others projects

--- vue/components/undo/undo.vue
+++ vue/components/undo/undo.vue
@@ -1,56 +1,26 @@
 <template>
-    <div id="undo">
+    <div class="undo">
         <div class="message-undo-container">
             <div class="message-undo" v-bind:class="{ visible: allowUndo }">
-                <a class="button button-undo" v-on:click="undo()">
-                    {{ "ripe_commons.pickers.undo" | locale }}
-                </a>
-                <a class="button button-undo button-back" v-on:click="undo()">
-                    {{ "ripe_commons.pickers.back_button" | locale }}
-                </a>
-                <span>
-                    {{ "ripe_commons.pickers.limited" | locale }}
-                    {{ "ripe_commons.pickers.back" | locale }}
-                </span>
+                <div class="button-container">
+                    <a class="button button-undo" v-on:click="undo()">
+                        {{ "ripe_commons.pickers.undo" | locale }}
+                    </a>
+                    <a class="button button-undo button-back" v-on:click="undo()">
+                        {{ "ripe_commons.pickers.back_button" | locale }}
+                    </a>
+                </div>
+                <div class="message-container">
+                    <span>
+                        {{ "ripe_commons.pickers.limited" | locale }}
+                        {{ "ripe_commons.pickers.back" | locale }}
+                    </span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-.pickers .message-undo-container {
-    overflow: hidden;
-}
-
-.pickers .message-undo {
-    background-color: #ececec;
-    border-radius: 5px;
-    display: block;
-    font-size: 11px;
-    line-height: 14px;
-    margin: 0px auto 0px auto;
-    max-width: 580px;
-    padding: 10px 20px 10px 30px;
-    text-align: left;
-    transform: translateY(-100%);
-    transition: transform 0.5s ease-in-out;
-}
-
-.pickers .message-undo.visible {
-    transform: translateY(0px);
-}
-
-.pickers .message-undo .button.button-undo {
-    cursor: pointer;
-    float: right;
-    font-size: 12px;
-    font-weight: bold;
-    margin-left: 20px;
-    margin-top: -10px;
-    padding: 10px 10px 10px 10px;
-    text-decoration: underline;
-}
-</style>
 <script>
 export const undo = {
     data: function() {
