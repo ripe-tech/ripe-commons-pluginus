d31816b047519690286894780d543fb773421f80
joamag <joamag@gmail.com>
2019-11-13 16:54:06 +0000

    Small fixes

--- vue/components/size.vue
+++ vue/components/size.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="size" v-bind:class="{ disabled: !enabled }">
         <div
-            class="button button-secondary button-size button-color"
+            class="button button-color button-size"
             v-bind:class="{ disabled: !enabled }"
             v-on:click="showModal()"
         >
