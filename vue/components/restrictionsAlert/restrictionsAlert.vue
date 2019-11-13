c72c5961693a795824053a93f24c01d84798d4e7
Daniel Blazeque <v-db@platforme.com>
2019-11-13 16:10:24 +0000

    fixing undo

--- vue/components/restrictionsAlert/restrictionsAlert.vue
+++ vue/components/restrictionsAlert/restrictionsAlert.vue
@@ -3,7 +3,7 @@
         <div class="message-restrictions-alert-container">
             <div class="message-restrictions-alert" v-bind:class="{ visible: visible }">
                 <div class="button-container">
-                    <a class="button button-restrictions-alert" v-on:click="restrictions-alert()">
+                    <a class="button button-restrictions-alert" v-on:click="undo()">
                         {{ "ripe_commons.restrictions_alert.restrictions_alert" | locale }}
                     </a>
                     <a class="button button-back" v-on:click="close()">
@@ -56,7 +56,10 @@ body.mobile .restrictions-alert .message-restrictions-alert .button-container {
 }
 
 .restrictions-alert .message-restrictions-alert-container .message-restrictions-alert .button {
+    align-items: center;
+    border: none;
     cursor: pointer;
+    display: flex;
     float: right;
     font-weight: bold;
     margin-left: 20px;
@@ -64,17 +67,23 @@ body.mobile .restrictions-alert .message-restrictions-alert .button-container {
     text-decoration: underline;
     user-select: none;
 }
+
+.restrictions-alert .message-restrictions-alert-container .message-restrictions-alert .button:hover {
+    background: #ffffff;
+    color: #151515;
+}
+
 </style>
 
 <script>
 export const restrictionsAlert = {
     data: function() {
         return {
-            visible: false
+            visible: true
         };
     },
     mounted: function() {
-        this.$bus.bind("restrictions-alert", (changes, newPart) => {
+        this.$bus.bind("restrictions", (changes, newPart) => {
             this.visible = changes.length > 0;
         });
     },
