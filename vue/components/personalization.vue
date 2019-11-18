af41cf3db1ba0ce95bec275230a15d084d45ccd8
Margarida Silva <t-ms@platforme.com>
2019-11-18 17:42:19 +0000

    pretty

--- vue/components/personalization.vue
+++ vue/components/personalization.vue
@@ -91,14 +91,26 @@ body.mobile .personalization ::v-deep .keyboard.text {
     width: 64px;
 }
 
-.personalization ::v-deep .keyboard.special .keyboard-row .keyboard-key.special .image-container img {
+.personalization
+    ::v-deep
+    .keyboard.special
+    .keyboard-row
+    .keyboard-key.special
+    .image-container
+    img {
     border-radius: 50%;
     height: 100%;
     object-fit: cover;
     width: 100%;
 }
 
-.personalization ::v-deep .keyboard.special .keyboard-row .keyboard-key.special .image-container .border {
+.personalization
+    ::v-deep
+    .keyboard.special
+    .keyboard-row
+    .keyboard-key.special
+    .image-container
+    .border {
     border: 2px solid #eaeaec;
     border-radius: 50%;
     height: calc(100% - 4px);
@@ -109,7 +121,13 @@ body.mobile .personalization ::v-deep .keyboard.text {
     width: calc(100% - 4px);
 }
 
-.personalization ::v-deep .keyboard.special .keyboard-row .keyboard-key.special.active .image-container .border {
+.personalization
+    ::v-deep
+    .keyboard.special
+    .keyboard-row
+    .keyboard-key.special.active
+    .image-container
+    .border {
     border-color: #ffffff;
     height: calc(100% - 10px);
     left: 3px;
