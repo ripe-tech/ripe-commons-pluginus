d4f5a2f563ae2f0eed197f542caf9e7b17c38194
João Magalhães <joamag@gmail.com>
2020-02-15 06:00:43 +0000

    Small renaming

--- js/ui/base-size/base-size.vue
+++ js/ui/base-size/base-size.vue
@@ -1,9 +1,9 @@
 <script>
 import { components } from "../../../vue";
 
-export const baseSize = {
-    extends: components.sizeForm.referenceMulti
+export const BaseSize = {
+    extends: components.sizeForm.ReferenceMulti
 };
 
-export default baseSize;
+export default BaseSize;
 </script>
