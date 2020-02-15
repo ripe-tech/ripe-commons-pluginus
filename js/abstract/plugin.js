dbd3bb3791fe17864ef708e07fe54225201f058a
joamag <joamag@hive.pt>
2020-02-15 12:28:51 +0000

    Added comments

--- js/abstract/plugin.js
+++ js/abstract/plugin.js
@@ -1,5 +1,12 @@
 import { Plugin } from "pluginus";
 
+/**
+ * Abstract class to be inherited from all the plugins
+ * that are considered to be part of RIPE Commons.
+ *
+ * Should contain the abstract functionality within the
+ * RIPE Commons functionality.
+ */
 class RipeCommonsPlugin extends Plugin {}
 
 export { RipeCommonsPlugin };
