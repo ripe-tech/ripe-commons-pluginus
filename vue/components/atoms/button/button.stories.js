0ce442f9be5e692245198f796b324bb994e12478
joamag <joamag@gmail.com>
2020-02-19 20:20:28 +0000

    Variant support for button

--- vue/components/atoms/button/button.stories.js
+++ vue/components/atoms/button/button.stories.js
@@ -1,5 +1,5 @@
 import { storiesOf } from "@storybook/vue";
-import { withKnobs, text } from "@storybook/addon-knobs";
+import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
 
 storiesOf("Atoms", module)
     .addDecorator(withKnobs)
@@ -7,11 +7,27 @@ storiesOf("Atoms", module)
         props: {
             text: {
                 default: text("Text", "Button")
+            },
+            variant: {
+                default: select(
+                    "Variant",
+                    {
+                        Unset: null,
+                        Default: "default"
+                    },
+                    null
+                )
+            },
+            active: {
+                default: boolean("Active", false)
             }
         },
         template: `
             <div>
-                <button-ripe v-bind:text="text" />
+                <button-ripe
+                    v-bind:text="text"
+                    v-bind:variant="variant"
+                    v-bind:active="active" />
             </div>
         `
     }));
--- vue/components/atoms/button/button.vue
+++ vue/components/atoms/button/button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button" v-on:click="onClick">
+    <button class="button" v-bind:class="classes" v-on:click="onClick">
         <slot>
             {{ text }}
         </slot>
@@ -8,7 +8,29 @@
 
 <style scoped>
 .button {
+    background-color: transparent;
+    border: 1px solid #cccccc;
+    cursor: pointer;
+    font-size: 14px;
     outline: none;
+}
+
+.button.button-variant-default {
+    border-radius: 2px 2px 2px 2px;
+    font-family: Lato, "Open Sans", arial, sans-serif;
+    font-weight: 500;
+    min-width: 120px;
+    padding: 6px 6px 6px 6px;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out;
+}
+
+.button:hover {
+    border: 1px solid #2d2d2d;
+}
+
+.button:active {
+    background-color: #2d2d2d;
+    color: #ffffff;
 }
 </style>
 
@@ -20,12 +42,24 @@ export const Button = {
             type: String,
             default: ""
         },
+        variant: {
+            type: String,
+            default: null
+        },
         active: {
             type: Boolean,
             default: false
         }
     },
-    computed: {},
+    computed: {
+        classes() {
+            const base = {
+                active: this.active
+            };
+            if (this.variant) base["button-variant-" + this.variant] = this.variant;
+            return base;
+        }
+    },
     methods: {
         onClick(event) {
             this.$emit("click", event);
