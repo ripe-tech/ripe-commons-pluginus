e8b3cec0093d733458effe70fc88cfb4d331b7d7
joamag <joamag@gmail.com>
2020-02-21 18:31:17 +0000

    Added icon support

--- vue/components/atoms/index.js
+++ vue/components/atoms/index.js
@@ -1,9 +1,11 @@
 import { Button } from "./button/button.vue";
+import { Icon } from "./icon/icon.vue";
 
 const install = Vue => {
     Vue.component("button-ripe", Button);
+    Vue.component("icon", Icon);
 };
 
-export { Button };
+export { Button, Icon };
 
 export default install;
