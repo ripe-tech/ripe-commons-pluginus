3a0403d9a909b99bf348d2d734085289b21946e9
3rdvision <kazgru@gmail.com>
2020-03-16 17:57:23 +0000

    Replicate alert logic from components-vue

--- vue/components/atoms/alert/alert.vue
+++ vue/components/atoms/alert/alert.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="alert" v-bind:style="alertStyle">
-        <div class="alert-content">
-            {{ message }}
+    <transition name="fade">
+        <div class="alert" v-bind:style="style" v-show="isVisible">
+            <global-events v-on:keydown.esc="handleGlobal" />
+            <div class="alert-content">
+                {{ textData }}
+            </div>
         </div>
-    </div>
+    </transition>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .alert {
     background-color: #151515;
-    box-shadow: 0 8px 50px 0 #61679426;
+    box-shadow: 0px 0px 16px rgba(45, 58, 70, 0.25);
     box-sizing: border-box;
     display: block;
     left: calc(50% - 127px);
-    opacity: 0.8;
     padding: 23px 25px 23px 25px;
     pointer-events: none;
     position: absolute;
     text-align: center;
     top: calc(50% - 57px);
-    transition: opacity 0.65s ease-in-out;
     user-select: none;
     width: 305px;
     z-index: 20;
+}
+
+.alert.fade-leave-active {
+    animation: fade-shrink-visibility 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) forwards;
 }
 
 .alert > .alert-content {
@@ -34,35 +39,102 @@
 export const Alert = {
     name: "alert",
     props: {
-        duration: {
+        text: {
+            type: String,
+            default: "hello world"
+        },
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        timeout: {
             type: Number,
-            default: 2000
+            default: 1000
+        },
+        topHeight: {
+            type: Number,
+            default: 150
+        },
+        globalEvents: {
+            type: Boolean,
+            default: true
+        }
+    },
+    computed: {
+        isVisible() {
+            return this.visibleData;
+        },
+        style() {
+            const base = {};
+            if (this.topHeight !== null) base.top = `${this.topHeight}px`;
+            return base;
         }
     },
     data: function() {
         return {
-            message: null,
-            alertOpacity: 0
+            visibleData: this.visible,
+            globalEventsData: this.globalEvents,
+            textData: this.text,
+            timeoutData: this.timeoutData,
+            timer: null,
+            key: true
         };
     },
-    computed: {
-        alertStyle() {
-            const base = {};
-            base.opacity = this.alertOpacity;
-            return base;
+    watch: {
+        text(value) {
+            this.textData = value;
+        },
+        visible(value) {
+            this.visibleData = value;
+        },
+        visibleData(value) {
+            this.$emit("update:visible", value);
         }
     },
     mounted: function() {
-        this.$bus.bind("alert", this.showAlert);
+        this.$bus.bind("alert", options => this.show(options));
+        this.$bus.bind("hide-global", () => {
+            if (!this.globalEventsData) return;
+            this.hide();
+        });
     },
     methods: {
-        showAlert(msg, duration) {
-            this.message = msg;
-            this.alertOpacity = 0.8;
-            clearTimeout(this.notificationMessageTimeout);
-            this.notificationMessageTimeout = setTimeout(() => {
-                this.alertOpacity = 0;
-            }, duration || this.duration);
+        show(options) {
+            const { text, timeout, globalEvents, reset = true } = options;
+
+            this.textData = text;
+            this.timeoutData = timeout || this.timeout;
+            this.globalEventsData = globalEvents || this.globalEvents;
+
+            if (this.visibleData) this.resetTimeout();
+            this.visibleData = true;
+
+            // alternate the key to force the component
+            // to be destroyed and mounted again
+            if (reset) this.reset();
+
+            this.timer = setTimeout(() => {
+                this.hide();
+                this.reset();
+            }, this.timeoutData);
+        },
+        hide() {
+            if (!this.visibleData) return;
+            this.visibleData = false;
+        },
+        handleClose() {
+            this.hide();
+        },
+        handleGlobal() {
+            if (!this.globalEvents) return;
+            if (this.visibleData) this.resetTimeout();
+            this.hide();
+        },
+        reset() {
+            this.key = !this.key;
+        },
+        resetTimeout() {
+            if (this.timer) clearTimeout(this.timer);
         }
     }
 };
