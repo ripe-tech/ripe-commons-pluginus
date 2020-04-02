31a94ea12ae80ef37cea762a7f525f67e40a8c60
joamag <joamag@hive.pt>
2020-04-02 19:08:54 +0100

    More component structure

--- vue/components/index.js
+++ vue/components/index.js
@@ -1,49 +1,27 @@
-import * as personalizationForm from "./personalization-form";
-import * as sizeForm from "./size-form";
-
 import Atoms from "./atoms";
+import Forms from "./forms";
 import Molecules from "./molecules";
 import Logic from "./logic";
-
-import { ComponentPlugin } from "./component-plugin";
 import { Personalization } from "./personalization";
 import { RestrictionsAlert } from "./restrictions-alert";
 import { Size } from "./size";
-import { Tab, Tabs } from "./tabs";
 
 const install = Vue => {
-    Vue.component("component-plugin", ComponentPlugin);
     Vue.component("personalization", Personalization);
     Vue.component("restrictions-alert", RestrictionsAlert);
     Vue.component("size", Size);
-    Vue.component("tab", Tab);
-    Vue.component("tabs", Tabs);
-
-    Vue.component("initials-images", personalizationForm.InitialsImages);
-    Vue.component("initials-inputs", personalizationForm.InitialsInputs);
 
     Vue.use(Atoms);
+    Vue.use(Forms);
     Vue.use(Molecules);
     Vue.use(Logic);
 };
 
 export * from "./atoms";
+export * from "./forms";
 export * from "./molecules";
 export * from "./logic";
 
-export {
-    install,
-    ComponentPlugin,
-    Personalization,
-    RestrictionsAlert,
-    Size,
-    Tab,
-    Tabs
-};
-
-export const InitialsImages = personalizationForm.InitialsImages;
-export const InitialsInputs = personalizationForm.InitialsInputs;
-
-export { personalizationForm, sizeForm };
+export { install, Personalization, RestrictionsAlert, Size };
 
 export default install;
--- vue/components/molecules/index.js
+++ vue/components/molecules/index.js
@@ -1,13 +1,19 @@
+import { ComponentPlugin } from "./component-plugin/component-plugin.vue";
 import { Keyboard } from "./keyboard/keyboard.vue";
 import { Modal } from "./modal/modal.vue";
+import { Tab } from "./tabs/tab.vue";
+import { Tabs } from "./tabs/tabs.vue";
 import { Thumbnails } from "./thumbnails/thumbnails.vue";
 
 const install = Vue => {
+    Vue.component("component-plugin", ComponentPlugin);
     Vue.component("keyboard", Keyboard);
     Vue.component("modal", Modal);
+    Vue.component("tab", Tab);
+    Vue.component("tabs", Tabs);
     Vue.component("thumbnails", Thumbnails);
 };
 
-export { Keyboard, Thumbnails };
+export { ComponentPlugin, Keyboard, Modal, Tab, Tabs, Thumbnails };
 
 export default install;
