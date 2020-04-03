8e0ca08300bd47487b7e1c837c493cb014cde971
joamag <joamag@hive.pt>
2020-04-03 10:33:14 +0100

    New ask error support

--- vue/mixins/utils.js
+++ vue/mixins/utils.js
@@ -18,6 +18,14 @@ const utilsMixin = {
             const { result } = await promise;
             return result;
         },
+        async askError(options) {
+            options.title = options.title || "Error";
+            options.message = options.message || "There was an error";
+            options.confirmButton = options.confirmButton || "Confirm";
+            options.cancelButton = options.cancelButton || null;
+            const result = await this.ask(options);
+            return result;
+        },
         readable(value) {
             return this.$options.filters.readable(value);
         },
