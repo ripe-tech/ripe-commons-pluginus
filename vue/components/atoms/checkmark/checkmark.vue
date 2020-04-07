eae804614f54afc069e0522ea51156c6e3a26415
Benedito0 <jorge.b.mauro@gmail.com>
2020-04-07 11:51:21 +0100

    checkmark component

---
+++ vue/components/atoms/checkmark/checkmark.vue
@@ -0,0 +1,68 @@
+<template>
+    <icon
+        class="checkmark"
+        v-bind:icon="icon"
+        v-bind:color="'white'"
+        v-bind:stroke-width="4.5"
+        v-bind:stroke-linecap="'round'"
+        v-bind:width="sizeNoPadding"
+        v-bind:height="sizeNoPadding"
+        v-bind:class="classes"
+        v-bind:style="style"
+    />
+</template>
+
+<style lang="scss" scoped>
+@import "css/variables.scss";
+
+.checkmark {
+    background-color: #c3c9cf;
+    box-sizing: border-box;
+}
+
+.checkmark.checked {
+    background-color: #1d2631;
+}
+</style>
+
+<script>
+export const Checkmark = {
+    name: "checkmark",
+    props: {
+        size: {
+            type: Number,
+            default: 14
+        },
+        value: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        icon() {
+            return this.value ? "ok" : "close";
+        },
+        padding() {
+            return parseInt(this.size / 4.5);
+        },
+        sizeNoPadding() {
+            return this.size - this.padding * 2;
+        },
+        style() {
+            return {
+                padding: `${this.padding}px`,
+                "border-radius": `${this.size}px`
+            };
+        },
+        classes() {
+            const base = {
+                unchecked: !this.value,
+                checked: this.value
+            };
+            return base;
+        }
+    }
+};
+
+export default Checkmark;
+</script>
--- vue/components/atoms/index.js
+++ vue/components/atoms/index.js
@@ -1,6 +1,7 @@
 import { Alert } from "./alert/alert.vue";
 import { Button } from "./button/button.vue";
 import { ButtonPlatforme } from "./button-platforme/button-platforme.vue";
+import { Checkmark } from "./checkmark/checkmark.vue";
 import { Icon } from "./icon/icon.vue";
 import { Loader } from "./loader/loader.vue";
 import { Thumbnail } from "./thumbnail/thumbnail.vue";
@@ -9,11 +10,12 @@ const install = Vue => {
     Vue.component("alert", Alert);
     Vue.component("button-ripe", Button);
     Vue.component("button-platforme", ButtonPlatforme);
+    Vue.component("checkmark", Checkmark);
     Vue.component("icon", Icon);
     Vue.component("loader", Loader);
     Vue.component("thumbnail", Thumbnail);
 };
 
-export { Alert, Button, ButtonPlatforme, Icon, Loader, Thumbnail };
+export { Alert, Button, ButtonPlatforme, Checkmark, Icon, Loader, Thumbnail };
 
 export default install;
