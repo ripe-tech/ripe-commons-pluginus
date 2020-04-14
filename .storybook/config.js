864f2da23d6d5ad6bfb9f7976279b0a963c8131c
joamag <joamag@hive.pt>
2020-04-14 13:32:20 +0100

    New comment operation

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,8 +5,12 @@ import { install as RipeCommonsPluginusVue, plugins } from "../vue";
 
 import "./styles.css";
 
+// initializes the event bus that will be used for
+// UI related communication between the components
 Vue.use(plugins.busPlugin);
 
+// initializes the main commons plugins structure
+// registering all of its components
 Vue.use(RipeCommonsPluginusVue);
 
 const req = require.context("../vue", true, /\.stories\.js$/);
