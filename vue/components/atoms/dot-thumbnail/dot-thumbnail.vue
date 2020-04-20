0131ac4413f83e0faad7d98ba0f4bcec5c076593
3rdvision <kazgru@gmail.com>
2020-04-20 09:27:38 +0100

    Pretty

--- vue/components/atoms/dot-thumbnail/dot-thumbnail.vue
+++ vue/components/atoms/dot-thumbnail/dot-thumbnail.vue
@@ -1,18 +1,14 @@
 <template>
     <div class="dot-container" v-on:click="showFrame">
-        <dot
-            v-bind:color="'dark'"
-            v-bind:subtle="!active"
-            v-bind:size="10"
-        />
+        <dot v-bind:color="'dark'" v-bind:subtle="!active" v-bind:size="10" />
     </div>
 </template>
 
 <style scoped>
 .dot-container {
+    cursor: pointer;
     display: inline-block;
     padding: 15px 15px 15px 15px;
-    cursor: pointer;
 }
 </style>
 
