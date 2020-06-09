f6398c33e1ed0c54f35945fe61d24e48b7b8f37d
Nuno Silva <nuno_nfss96@hotmail.com>
2020-06-09 11:10:30 +0100

    Crude style

--- vue/components/molecules/messages-alert/messages-alert.vue
+++ vue/components/molecules/messages-alert/messages-alert.vue
@@ -14,7 +14,39 @@
 </template>
 
 <style scoped>
+.messages-alert {
+    display: flex;
+    align-items: center;
+    background-color: "#ffffff";
+    border-radius: 6px;
+    box-shadow: 0px 8px 12px 0px rgba(98, 110, 117, 0.15);
+    padding: 16px 20px 16px 20px;
+}
 
+
+.messages-alert > .messages{
+    flex: 1;
+    background-color: lightgreen;
+}
+
+.messages-alert > .buttons{
+    display: inline-block;
+    background-color: lightblue;
+}
+
+
+
+
+.messages-alert > .buttons > .undo,
+.messages-alert > .buttons > .redo
+{
+    display: inline-block;
+    text-decoration: underline;
+}
+
+.messages-alert > .buttons > .undo {
+    
+}
 
 
 
