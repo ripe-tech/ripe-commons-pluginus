1d7db198e93e230cb92b6b16539d0c7342baea57
Nuno Silva <nuno_nfss96@hotmail.com>
2020-06-16 13:59:29 +0100

    Testing ripe plugins in tests

--- vue/test/components/restrictions-alert/restrictions-alert.test.js
+++ vue/test/components/restrictions-alert/restrictions-alert.test.js
@@ -5,9 +5,19 @@ describe("Restrictions Alert", () => {
     it("Closes on Undo", () => {
         const component = base.getComponent("RestrictionsAlert");
         
-        console.log("\n\n--------testing stuff:\n");
-        console.log("ripe:", component.vm.$ripe)
-        console.log("bus:", component.vm.$bus)
+
+
+        console.log("\n\n--------\n", "testing stuff:\n");
+
+        // component.vm.$ripe.plugins[0] is the "RestrictionsPlugin"
+        component.vm.$ripe.plugins[0].bind("restrictions", (...args) =>
+            component.vm.$bus.trigger("restrictions", ...args)
+        );
+        component.vm.$bus.bind("restrictions", (changes, partSet) =>
+            console.log("Testing restrictions event:", changes, partSet)
+        ); // TODO remove
+
+        component.vm.$ripe.plugins[0]._applyRestrictions("name test", "value test");
 
 
 
