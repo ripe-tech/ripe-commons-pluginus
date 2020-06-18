d678b05f99607010d4883f2f912c6c5383c6e732
Nuno Silva <nuno_nfss96@hotmail.com>
2020-06-18 10:44:13 +0100

    Reusing "restrictions-alert" code in "messages-alert"

--- vue/components/molecules/messages-alert/messages-alert.vue
+++ vue/components/molecules/messages-alert/messages-alert.vue
@@ -1,104 +1,71 @@
 <template>
-    <transition
-        name="slide-transition"
-        v-on:before-enter="onSlideTransitionBeforeEnter"
-        v-on:after-leave="onSlideTransitionAfterLeave"
-    >
-        <div class="messages-alert" v-show="visible">
-            <div class="messages">
-                <div class="message" v-for="(message, index) in visibleMessages" v-bind:key="index">
-                    <div class="name">
-                        {{ `${message.name}:` }}
-                    </div>
-                    <div class="value">
-                        {{ message.value }}
-                    </div>
+    <div class="restrictions-alert">
+        <div class="message-restrictions-alert-container">
+            <div class="message-restrictions-alert" v-bind:class="{ visible: messages.length > 0 }">
+                <div class="button-container">
+                    <a class="button button-restrictions-alert" v-on:click="undo">
+                        {{ "ripe_commons.restrictions_alert.restrictions_alert" | locale }}
+                    </a>
+                    <a class="button button-back" v-on:click="close">
+                        {{ "ripe_commons.restrictions_alert.close_button" | locale }}
+                    </a>
                 </div>
-            </div>
-            <div class="buttons">
-                <div class="undo" v-on:click="undo">
-                    {{ "ripe_commons.messages_alert.undo_button" | locale }}
-                </div>
-                <div class="close" v-on:click="close">
-                    {{ "ripe_commons.messages_alert.close_button" | locale }}
+                <div class="message-container">
+                    <span>
+                        {{ "ripe_commons.restrictions_alert.limited" | locale }}
+                        {{ "ripe_commons.restrictions_alert.back" | locale }}
+                    </span>
                 </div>
             </div>
         </div>
-    </transition>
+    </div>
 </template>
 
 <style scoped>
-.messages-alert {
-    align-items: center;
-    background-color: #ffffff;
-    border-radius: 6px;
-    box-shadow: 0px 8px 12px 0px rgba(98, 110, 117, 0.15);
+.restrictions-alert .message-restrictions-alert-container {
     display: flex;
-    left: 0px;
-    margin: auto;
-    padding: 16px 20px 16px 20px;
-    position: fixed;
-    right: 0px;
-    top: 90px;
-    transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1),
-        opacity 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
-    width: fit-content;
-    z-index: 3;
+    flex-direction: row-reverse;
+    overflow: hidden;
 }
 
-.messages-alert.slide-transition-enter,
-.messages-alert.slide-transition-leave-to {
-    opacity: 0;
-    top: 0px;
+.restrictions-alert .message-restrictions-alert-container .message-restrictions-alert {
+    align-items: center;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: center;
+    padding: 10px 20px 10px 30px;
+    text-align: left;
+    z-index: 2;
 }
 
-.messages-alert.slide-transition-enter-to {
-    top: 90px;
+body.mobile .restrictions-alert .message-restrictions-alert-container .message-restrictions-alert,
+body.tablet .restrictions-alert .message-restrictions-alert-container .message-restrictions-alert {
+    flex-direction: column-reverse;
+    max-width: 70%;
 }
 
-.messages-alert > .messages {
-    margin: 0px 16px 0px 0px;
+.restrictions-alert .message-restrictions-alert .button-container {
+    display: flex;
+    flex-direction: row;
 }
 
-.messages-alert > .messages > .message {
-    margin: 0px 0px 10px 0px;
+body.tablet .restrictions-alert .message-restrictions-alert .button-container,
+body.mobile .restrictions-alert .message-restrictions-alert .button-container {
+    justify-content: flex-end;
+    width: 100%;
 }
 
-.messages-alert > .messages > .message:last-child {
-    margin: 0px 0px 0px 0px;
-}
-
-.messages-alert > .messages > .message > .name,
-.messages-alert > .messages > .message > .value {
-    display: inline-block;
-}
-
-.messages-alert > .messages > .message > .name {
-    font-weight: 600;
-    padding: 0px 2px 0px 0px;
-    text-transform: capitalize;
-}
-
-.messages-alert > .buttons > .undo,
-.messages-alert > .buttons > .close {
-    color: #5d5d5d;
+.restrictions-alert .message-restrictions-alert-container .message-restrictions-alert .button {
+    align-items: center;
+    border: none;
     cursor: pointer;
-    display: inline-block;
-    font-size: 15px;
+    display: flex;
+    float: right;
     font-weight: 600;
-    margin: 0px 5px 0px 5px;
+    margin-left: 20px;
+    padding: 10px 10px 10px 10px;
     text-decoration: underline;
     user-select: none;
-}
-
-.messages-alert > .buttons > .undo:hover,
-.messages-alert > .buttons > .close:hover {
-    color: #000000;
-}
-
-.messages-alert > .buttons > .undo:active,
-.messages-alert > .buttons > .close:active {
-    color: #737373;
 }
 </style>
 
@@ -107,15 +74,8 @@ export const MessagesAlert = {
     name: "messages-alert",
     data: function() {
         return {
-            messages: [],
-            allowVisible: true,
-            visibleMessages: []
+            messages: []
         };
-    },
-    computed: {
-        visible() {
-            return this.messages.length > 0 && this.allowVisible;
-        }
     },
     created: function() {
         this.onMessage = this.$bus.bind("message", (name, value) => {
@@ -134,18 +94,11 @@ export const MessagesAlert = {
         if (this.onMessage) this.$bus.unbind("message", this.onMessage);
     },
     methods: {
-        onSlideTransitionBeforeEnter() {
-            this.visibleMessages = [...this.messages];
-        },
-        onSlideTransitionAfterLeave() {
-            this.allowVisible = true;
-        },
         undo() {
             this.close();
             this.$bus.trigger("undo");
         },
         close() {
-            this.allowVisible = !this.visible;
             this.messages = [];
         }
     }
