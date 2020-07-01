e5f3276a88b54ddb2403fdf8fa6d83508bee1921
3rdvision <kazgru@gmail.com>
2020-07-01 17:41:11 +0100

    Add semantic classes to generic personalization

--- vue/components/forms/personalization-form/reference.vue
+++ vue/components/forms/personalization-form/reference.vue
@@ -18,6 +18,7 @@
                     v-bind:key="type"
                 >
                     <select-ripe
+                        v-bind:class="(`properties.${type}`, readable(type)) | locale"
                         v-bind:placeholder="`ripe_commons.personalization.select.${type}` | locale"
                         v-bind:options="options"
                         v-bind:value="propertiesData[group][type]"
@@ -29,6 +30,7 @@
                     v-bind:header-size="'large'"
                 >
                     <input-ripe
+                        class="add-initials"
                         v-bind:placeholder="'ripe_commons.personalization.add_initials' | locale"
                         v-bind:value.sync="initialsText[group]"
                     />
