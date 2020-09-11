29b06069567fb22b35daa1b7b4e95850777a7550
Nuno Silva <nuno_nfss96@hotmail.com>
2020-09-11 17:22:11 +0100

    Improved plugin init

--- vue/components/organisms/suggestions/suggestions.vue
+++ vue/components/organisms/suggestions/suggestions.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="suggestions">
-        <component v-bind:is="form" ref="form" />
+        <component v-bind:is="form" ref="form" v-bind:key="formKey" />
     </div>
 </template>
 
@@ -14,11 +14,47 @@ export const Suggestions = {
             form: null
         };
     },
+    computed: {
+        formKey() {
+            return this.brand + "." + this.model + "." + this.counter;
+        }
+    },
     created: async function() {
-        const plugins = await this.manager.getPluginsByCapability("suggestions");
+        this.$bus.bind("post_config", async config => {
+            // in case there's no valid config for this post operation
+            // returns control flow immediately, should not happen
+            if (!config) {
+                return;
+            }
 
-        const form = await plugins[0].getSuggestionsComponent();
-        this.form = form;
+            // gathers the complete set of plugins registered for suggestions
+            // sorts them according to the brand fitting property and then
+            // retrieves the first plugin (best candidate)
+            const plugins = (await this.manager.getPluginsByCapability("suggestions"))
+                .filter(plugin => !plugin.meta.brand || plugin.meta.brand === this.brand)
+                .map(plugin => (plugin.meta.brand === this.brand ? [1, plugin] : [0, plugin]))
+                .sort((a, b) => b[0] - a[0])
+                .map(v => v[1]);
+            const plugin = plugins.length ? plugins[0] : null;
+
+            // in case there's no valid size plugin available raises an error
+            // indicating the issue with size loading
+            if (!plugin) {
+                throw new Error(`No size component found for ${this.brand}`);
+            }
+
+            // retrieves its suggestions component setting it as the form
+            // component for this specific suggestions scenario
+            const form = await plugin.getSuggestionsComponent();
+
+            // increments the cache invalidation counter, internal hack to enable
+            // proper computation of computed values
+            this.counter += 1;
+
+            // sets the form for the current component, sets it as enable and hidden
+            // by default (initial state)
+            this.form = form;
+        });
     }
 };
 
