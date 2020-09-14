060662dd086c68591e8dc975fa2fd042b4dbd6b0
Nuno Silva <nuno_nfss96@hotmail.com>
2020-09-14 09:36:06 +0100

    Generate list from queries prop

--- vue/components/forms/suggestions-form/suggestions-list.vue
+++ vue/components/forms/suggestions-form/suggestions-list.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="suggestions-list">
-        List of suggestions
+        <div class="suggestion" v-for="suggestion in suggestions" v-bind:key="suggestion.model">
+            <image-ripe class="thumbnail" v-bind:src="suggestion.imgUrl" />
+            <div class="model">
+                {{ suggestion.model }}
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.suggestions-list > .suggestion,
+.suggestions-list > .suggestion > .thumbnail,
+.suggestions-list > .suggestion > .model {
+    display: inline-block;
+    height: 100px;
+    width: 100px;
+}
+</style>
 
 <script>
 export const SuggestionsList = {
@@ -13,6 +26,27 @@ export const SuggestionsList = {
         queries: {
             type: Array,
             default: () => []
+        }
+    },
+    data: function() {
+        return {
+            suggestions: []
+        };
+    },
+    created: async function() {
+        await this.loadSuggestions();
+    },
+    methods: {
+        async loadSuggestions() {
+            this.suggestions = await Promise.all(
+                this.queries.map(async _query => {
+                    const spec = this.$ripe._queryToSpec(_query);
+                    const { query } = await this.$ripe.configInfoP(spec);
+                    console.log("confQuery", query);
+
+                    return { ...spec, imgUrl: `${this.$ripe.url}compose?${query}` };
+                })
+            );
         }
     }
 };
