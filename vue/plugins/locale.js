e336124833fd3ef39eb031786a450d850e0d50de
Gabriel Candal <gcc@platforme.com>
2020-11-05 17:58:37 +0000

    Pretty

--- vue/plugins/locale.js
+++ vue/plugins/locale.js
@@ -11,23 +11,30 @@ export const localePlugin = {
             data: function() {
                 return {
                     currentLocale: null
+                };
+            },
+            computed: {
+                locale() {
+                    return (value, defaultValue, locale = null, fallback = true) => {
+                        return toLocale(
+                            value,
+                            defaultValue,
+                            locale || this.currentLocale,
+                            fallback
+                        );
+                    };
                 }
             },
             mounted: function() {
                 localePlugin.owner.bind("post_set_locale", locale => (this.currentLocale = locale));
                 this.currentLocale = localePlugin.locale;
-            },
-            computed: {
-                locale() {
-                    return (value, defaultValue, locale = null, fallback = true) => {
-                        return toLocale(value, defaultValue, locale || this.currentLocale, fallback);
-                    };
-                }
             }
         });
 
         Vue.filter("locale", function(value, defaultValue, locale, fallback = true) {
-            console.warn("Using 'locale' as a filter has been deprecated, please use it as a function instead.");
+            console.warn(
+                "Using 'locale' as a filter has been deprecated, please use it as a function instead."
+            );
             return toLocale(value, defaultValue, locale, fallback);
         });
 
