b0160a81aa6ce5f5943167f079dfb9fe3328a303
Margarida Silva <ms@platforme.com>
2020-11-23 11:46:11 +0000

    Stop progragation of click on scroll arrows, so that they don't open the customization panel

--- vue/components/organisms/pickers/pickers.vue
+++ vue/components/organisms/pickers/pickers.vue
@@ -13,7 +13,7 @@
                 class="button-scroll button-scroll-left button-scroll-parts button-scroll-parts-left"
                 v-if="enableScrollParts"
                 v-show="scrollParts"
-                v-on:click="slideLeftParts"
+                v-on:click.stop="slideLeftParts"
             />
             <ul class="parts-container" ref="partsPicker">
                 <slot name="before-buttons-parts">
@@ -74,7 +74,7 @@
                 class="button-scroll button-scroll-right button-scroll-parts button-scroll-parts-right"
                 v-if="enableScrollParts"
                 v-show="scrollParts"
-                v-on:click="slideRightParts"
+                v-on:click.stop="slideRightParts"
             />
         </div>
         <div class="materials-wrapper">
