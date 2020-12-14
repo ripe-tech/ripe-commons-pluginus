63ebe2a6abc89c791293a24773e8707c4a20744c
Gabriel Candal <gcc@platforme.com>
2020-12-14 16:25:06 +0000

    ModelLocaleResolverPlugin first test

--- test/locale/model-locale-resolver.js
+++ test/locale/model-locale-resolver.js
@@ -1,8 +1,16 @@
-const ripeCommons = require("../..");
+const assert = require("assert");
 
 describe("ModelLocaleResolverPlugin", function() {
-    it("should be able to run simple tests", () => {
-        const plugin = new ripeCommons.ModelLocaleResolverPlugin();
-        assert.strictEqual("hello", plugin.localeColor("red"));
+    it("localeColor", async () => {
+        const localePlugin = await global.manager.getPluginByName("LocalePlugin");
+        localePlugin.setLocale("en_us");
+        localePlugin.setLocaleMap({
+            en_us: {
+                "colors.red": "red"
+            }
+        });
+
+        const plugin = await global.manager.getPluginByName("ModelLocaleResolverPlugin");
+        assert.strictEqual("red", plugin.localeColor("red"));
     });
 });
--- test/setup.js
+++ test/setup.js
@@ -2,3 +2,9 @@ require("core-js");
 require("regenerator-runtime/runtime");
 require("jsdom-global")(undefined, { url: "https://mock.ripe-pulse.platforme.com/" });
 global.fetch = require("node-fetch");
+
+const ripeCommons = require("..");
+const pluginus = require("pluginus");
+
+global.manager = pluginus.manager;
+ripeCommons.registerPlugins(pluginus.manager);
