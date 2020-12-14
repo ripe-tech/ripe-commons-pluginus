d6706925716738011fff9833ca4837678df861c4
Gabriel Candal <gcc@platforme.com>
2020-12-14 16:26:44 +0000

    Fix product name

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 require("core-js");
 require("regenerator-runtime/runtime");
-require("jsdom-global")(undefined, { url: "https://mock.ripe-pulse.platforme.com/" });
+require("jsdom-global")(undefined, { url: "https://mock.ripe-commons-pluginus.platforme.com/" });
 global.fetch = require("node-fetch");
 
 const ripeCommons = require("..");
--- vue/test/setup.js
+++ vue/test/setup.js
@@ -1,3 +1,3 @@
 require("core-js");
-require("jsdom-global")(undefined, { url: "https://mock.ripe-pulse.platforme.com/" });
+require("jsdom-global")(undefined, { url: "https://mock.ripe-commons-pluginus.platforme.com/" });
 global.fetch = require("node-fetch");
