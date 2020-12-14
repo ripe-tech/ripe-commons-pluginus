74b708a8cfe669e964ec57b3de1102e3573bf2fb
Gabriel Candal <gcc@platforme.com>
2020-12-14 16:42:07 +0000

    localeColor bug unit test

--- test/locale/model-locale-resolver.js
+++ test/locale/model-locale-resolver.js
@@ -6,11 +6,15 @@ describe("ModelLocaleResolverPlugin", function() {
         localePlugin.setLocale("en_us");
         localePlugin.setLocaleMap({
             en_us: {
-                "colors.red": "Red"
+                "colors.red": "Red",
+                "colors.rubber.red": "Rubber Red",
+                "colors.sole.rubber.red": "Sole Rubber Red"
             }
         });
 
         const plugin = await global.manager.getPluginByName("ModelLocaleResolverPlugin");
         assert.strictEqual("Red", plugin.localeColor("red"));
+        assert.strictEqual("Rubber Red", plugin.localeColor("red", { part: "front", material: "rubber" }));
+        assert.strictEqual("Sole Rubber Red", plugin.localeColor("red", { part: "sole", material: "rubber" }));
     });
 });
