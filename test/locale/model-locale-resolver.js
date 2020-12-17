fd5071ccf77dc6866d41df4d3052b472b08f5c09
Gabriel Candal <gcc@platforme.com>
2020-12-17 11:44:46 +0000

    Remove work from setup

--- test/locale/model-locale-resolver.js
+++ test/locale/model-locale-resolver.js
@@ -1,19 +1,29 @@
+require("../setup");
+
 const assert = require("assert");
+const pluginus = require("pluginus");
+
+const ripeCommons = require("../..");
 
 describe("ModelLocaleResolverPlugin", function() {
     before(async function() {
-        const localePlugin = await global.manager.getPluginByName("LocalePlugin");
+        const manager = new pluginus.PluginManager();
+        ripeCommons.registerPlugins(manager);
+
+        const localePlugin = await manager.getPluginByName("LocalePlugin");
         localePlugin.setLocale("en_us");
         localePlugin.setLocaleMap({
             en_us: {
                 "colors.red": "Red"
             }
         });
+
+        this.manager = manager;
     });
 
     describe("#localeColor()", function() {
         it("should make a simple color translation", async () => {
-            const plugin = await global.manager.getPluginByName("ModelLocaleResolverPlugin");
+            const plugin = await this.ctx.manager.getPluginByName("ModelLocaleResolverPlugin");
             assert.strictEqual("Red", plugin.localeColor("red"));
         });
     });
