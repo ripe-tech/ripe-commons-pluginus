1b1b16eda23baf5cb35ab229a2a277a9fa671cf1
João Magalhães <joamag@gmail.com>
2022-01-12 17:57:30 +0000

    docs: added new comments

--- vue/components/forms/personalization-form/initials-images.vue
+++ vue/components/forms/personalization-form/initials-images.vue
@@ -272,10 +272,15 @@ export const InitialsImages = {
             // so sends an event
             if (!this.groups.every(group => Object.keys(this.srcs).includes(group))) return;
 
-            // does not send load images if the frame of the image
-            // loaded is outdated
+            // incase the frame that was loaded is not longer the
+            // latest one ignores the load operation, retuning the
+            // control flow immediately
             const queryParams = this.$ripe._unpackQuery(src);
             if (this.frame && queryParams.frame !== this.frame) return;
+
+            // triggers the load images operation indicating that
+            // all of the images for the current configuration
+            // have been loaded
             this.$emit("load:images");
         }
     }
