f6a4766886fe433418569908426c8beac687dfcd
Margarida Silva <ms@platforme.com>
2022-02-15 15:24:17 +0000

    revert: node-fetch bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,7 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
-*
+* Revert `node-fetch` bump back to @2
 
 ## [0.21.0] - 2022-02-15
 
--- vue/test/setup.js
+++ vue/test/setup.js
@@ -1,8 +1,3 @@
 require("core-js");
 require("jsdom-global")(undefined, { url: "https://mock.ripe-commons-pluginus.platforme.com/" });
-
-// node-fetch uses ESM with an async import and so we need to patch
-// the current import operation to make it compatible with the new
-// async ESM driven approach
-const fetchPromise = import("node-fetch").then(mod => mod.default);
-global.fetch = (...args) => fetchPromise.then(fetch => fetch(...args));
+global.fetch = require("node-fetch");
