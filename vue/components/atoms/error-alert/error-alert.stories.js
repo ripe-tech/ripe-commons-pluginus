ed3bbab9fdb14a1c7a43150d77678e8f598d49e1
3rdvision <kazgru@gmail.com>
2022-03-08 18:15:11 +0000

    refactor: move mockError definition

--- vue/components/atoms/error-alert/error-alert.stories.js
+++ vue/components/atoms/error-alert/error-alert.stories.js
@@ -1,8 +1,6 @@
 import { storiesOf } from "@storybook/vue";
 import { withKnobs, boolean } from "@storybook/addon-knobs";
 
-const mockError = new TypeError("Mock error!", "mock.js");
-
 storiesOf("Components/Atoms/Error Alert", module)
     .addDecorator(withKnobs)
     .add("Error Alert", () => ({
@@ -16,7 +14,7 @@ storiesOf("Components/Atoms/Error Alert", module)
         },
         data: function() {
             return {
-                mockError: mockError
+                mockError: new TypeError("Mock error!", "mock.js")
             };
         },
         template: `
