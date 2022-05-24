82cb887a569f9363a94c61195338e03d37c98566
Margarida Silva <ms@platforme.com>
2022-05-24 12:43:05 +0100

    fix: esling config hive problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: ["hive/vue", "hive/prettier"],
     rules: {
-        "vue/no-v-html": ["off"]
+        "vue/no-v-html": ["off"],
+        "vue/no-child-content": ["off"]
     },
     env: {
         mocha: true
