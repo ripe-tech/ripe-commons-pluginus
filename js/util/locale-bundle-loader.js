86afeb7402dce5c47f5d3436a79a91d71c936fae
João Magalhães <joamag@gmail.com>
2022-08-01 15:56:56 +0100

    docs: small comments added

--- js/util/locale-bundle-loader.js
+++ js/util/locale-bundle-loader.js
@@ -6,6 +6,9 @@ export class LocaleBundleLoaderPlugin extends RipeCommonsPlugin {
         this.ripeProvider = await this.owner.getPluginByCapability("ripe-provider");
         this.loadedBundles = [];
 
+        // registers for the locale changed event so
+        // that we can try to load the locale bundle in
+        // case it's currently not registered
         this.onLocaleChangedWatch = this.owner.bind(
             "locale_changed",
             async locale => await this.onLocaleChanged(locale)
@@ -27,7 +30,7 @@ export class LocaleBundleLoaderPlugin extends RipeCommonsPlugin {
             await this.loadLocaleBundle(locale);
         } else {
             // in case ripe provider's ripe instance is not yet accessible
-            // then wait for the loading of the plugin to make sure that
+            // then waits for the loading of the plugin to make sure that
             // the store is available
             const postLoadBind = this.owner.bind("post_load", () => {
                 this.owner.unbind("post_load", postLoadBind);
